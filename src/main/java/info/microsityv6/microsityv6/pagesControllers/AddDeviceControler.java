ef3d2d4f5a97c9a1c8ca841de9e73a3ca93a9974
Panker-RDP <Panker-RDP@192.168.1.2>
2016-10-11 23:53:08 +0300

    Added not too much. Buth i think tomorrou i can finish all.

--- src/main/java/info/microsityv6/microsityv6/pagesControllers/AddDeviceControler.java
+++ src/main/java/info/microsityv6/microsityv6/pagesControllers/AddDeviceControler.java
@@ -1,16 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package info.microsityv6.microsityv6.pagesControllers;
 
 import info.microsityv6.microsityv6.entitys.Counter;
 import info.microsityv6.microsityv6.entitys.ESP;
 import info.microsityv6.microsityv6.entitys.Kit;
+import info.microsityv6.microsityv6.entitys.Log;
 import info.microsityv6.microsityv6.entitys.Pin;
 import info.microsityv6.microsityv6.enums.CounterType;
 import info.microsityv6.microsityv6.enums.DeviceType;
+import info.microsityv6.microsityv6.enums.LoggerLevel;
 import info.microsityv6.microsityv6.facades.KitFacade;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
@@ -21,23 +18,19 @@ import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 
-/**
- *
- * @author Panker-RDP
- */
 @Named(value = "addDeviceController")
 @SessionScoped
-public class addDeviceController extends PageController implements Serializable {
+public class AddDeviceControler extends PageController implements Serializable {
 
     @EJB
     KitFacade kitFacade;
 
-    private String addedKitHexId;
+    private String addedKitHexId = "";
     private boolean showInfo;
     private Kit addedKit = new Kit();
     private List<Kit> kits;
 
-    public addDeviceController() {
+    public AddDeviceControler() {
 
     }
 
@@ -50,6 +43,7 @@ public class addDeviceController extends PageController implements Serializable 
     }
 
     public String addKit() {
+        boolean added = false;
         if (kitFacade.findAll().isEmpty()) {
             makeDummy();
         } else {
@@ -62,20 +56,91 @@ public class addDeviceController extends PageController implements Serializable 
                         for (Pin pin : esp.getPins()) {
                             if (pin.getType().equals(DeviceType.COUNTER)) {
                                 Counter addedCounter = new Counter();
-                                addedCounter.setCounterType(CounterType.WATER);
+                                addedCounter.setTitle("Новый " + pin.getCounterType().getAbout());
+                                addedCounter.setCounterType(pin.getCounterType());
                                 addedCounter.setEsp_id(esp.getEspId());
-                                addedCounter.setPin(esp.getPins().indexOf(pin));
+                                addedCounter.setPin(pin.getPin_num());
                                 super.getCurrentFacility().getCounters().add(addedCounter);
-
+                                super.saveChanges();
+                                added = true;
+                                addedKit = kit;
+                                addedKit.setSold(true);
+                                addedKitHexId = "";
                             }
                         }
                     }
                 }
             }
         }
-
+        if (added) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Набор успешно добавлен. Перейдите к настройкам устройств"));
+            kitFacade.edit(addedKit);
+            logFacade.create(new Log(LoggerLevel.INFO, super.getCurrent().getMainEmail() + " add ne kit with ID: " + addedKit.getKitHexId()));
+            return "";
+        }
         FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("ID не найден в базе, проверьте правильность ввода"));
         return "";
+    }
+
+    private void makeDummy() {
+        kits = new ArrayList<>();
+        Kit firstKit = new Kit();
+        //1
+        ESP esp1 = new ESP();
+        esp1.setEspId("ESP_18:fe:34:a6:a0:e4");
+        Pin pin1 = new Pin();
+        pin1.setPin_num(2);
+        pin1.setType(DeviceType.COUNTER);
+        pin1.setCounterType(CounterType.WATT);
+        esp1.getPins().add(pin1);
+        firstKit.addEsp(esp1);
+        //+
+        ESP esp2 = new ESP();
+        esp2.setEspId("ESP_18:fe:34:da:8c:32");
+        Pin pin2 = new Pin();
+        pin2.setPin_num(2);
+        pin2.setType(DeviceType.COUNTER);
+        pin2.setCounterType(CounterType.WATER);
+        esp2.getPins().add(pin2);
+        firstKit.addEsp(esp2);
+        genKitId(firstKit);
+        kitFacade.create(firstKit);
+        logFacade.create(new Log(LoggerLevel.INFO, "System create new kit with ID: " + firstKit.getKitHexId()));
+        //2
+        Kit secondKit = new Kit();
+        esp1 = new ESP();
+        esp1.setEspId("ESP_18:fe:34:da:8a:77");
+        pin1 = new Pin();
+        pin1.setPin_num(2);
+        pin1.setType(DeviceType.COUNTER);
+        pin1.setCounterType(CounterType.WATT);
+        esp1.getPins().add(pin1);
+        secondKit.addEsp(esp1);
+        genKitId(secondKit);
+        kitFacade.create(secondKit);
+        logFacade.create(new Log(LoggerLevel.INFO, "System create new kit with ID: " + secondKit.getKitHexId()));
+    }
+
+    private void genKitId(Kit editedKit) {
+        editedKit.genKitHexId();
+        boolean mach = true;
+        if (kits.isEmpty()) {
+            kits.add(editedKit);
+            return;
+        }
+        while (mach) {
+            for (Kit kit : kits) {
+                if (kit.getKitHexId().equalsIgnoreCase(editedKit.getKitHexId())) {
+                    editedKit.genKitHexId();
+                    mach = true;
+                    break;
+                } else {
+                    mach = false;
+                }
+            }
+        }
+        kits.add(editedKit);
+
     }
 
     public void checkMatches() {
@@ -103,55 +168,5 @@ public class addDeviceController extends PageController implements Serializable 
 
     public void setAddedKit(Kit addedKit) {
         this.addedKit = addedKit;
-    }
-
-    private void makeDummy() {
-        Kit firstKit = new Kit();
-        ESP esp1 = new ESP();
-        esp1.setEspId("ESP_18:fe:34:a6:a0:e4");
-        Pin pin1 = new Pin();
-        pin1.setPin_num(2);
-        pin1.setType(DeviceType.COUNTER);
-        esp1.getPins().add(pin1);
-        firstKit.addEsp(esp1);
-        ESP esp2 = new ESP();
-        esp2.setEspId("ESP_18:fe:34:da:8c:32");
-        Pin pin2 = new Pin();
-        pin2.setPin_num(2);
-        pin2.setType(DeviceType.COUNTER);
-        esp2.getPins().add(pin2);
-        firstKit.addEsp(esp2);
-        Kit secondKit = new Kit();
-        esp1 = new ESP();
-        pin1 = new Pin();
-        pin1.setPin_num(2);
-        pin1.setType(DeviceType.COUNTER);
-        esp1.getPins().add(pin1);
-        secondKit.addEsp(esp2);
-        if (kitFacade.findAll() == null) {
-            kits = new ArrayList<>();
-        } else {
-            genKitId(firstKit);
-            genKitId(secondKit);
-        }
-        kitFacade.create(firstKit);
-        kitFacade.create(secondKit);
-    }
-
-    private Kit genKitId(Kit editedKit) {
-        editedKit.genKitHexId();
-        boolean mach = true;
-        while (mach) {
-            for (Kit kit : kits) {
-                if (kit.getKitHexId().equalsIgnoreCase(editedKit.getKitHexId())) {
-                    editedKit.genKitHexId();
-                    mach = true;
-                    break;
-                } else {
-                    mach = false;
-                }
-            }
-        }
-        return editedKit;
     }
 }
